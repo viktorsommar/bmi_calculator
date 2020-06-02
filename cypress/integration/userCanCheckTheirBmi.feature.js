1af8339a4dd046ea2a9129925dfe8e0b6af710bb
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-02 18:35:39 +0200

    Doing some further testing with Cypress

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,5 +1,6 @@
 describe('User can check their bmi', () => {
     it('User can enter weight and height', () => {
         cy.visit('http://localhost:3001')
+        cy.contains('BMI Calculator')
     })
 })
